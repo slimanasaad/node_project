a5134cf12cc2dd2d2699ba924a5dac0f82fc6179
slimanasaad <106686625+slimanasaad@users.noreply.github.com>
2024-06-08 02:14:49 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,13 +10,6 @@ const connection = mysql.createConnection({
   database : 'id22126002_node_project'
 });
 
-connection.connect((error)=>{
-    if(error){
-        console.log("error")
-    }else{
-        console.log("mysql connected")
-    }
-})
 
 
 app.get('/', (req, res) => {
