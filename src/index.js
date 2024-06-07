fe26c72bd12798b57dc15041c63a61b930b9ac28
slimanasaad <106686625+slimanasaad@users.noreply.github.com>
2024-06-07 19:43:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,6 @@
 const express = require('express');
+var _ = require('underscore');
 const app = express();
-// const { MongoClient } = require('mongodb'); // Correct import syntax
-// async function main() {
-//     const uri = "mongodb+srv://slimanas3d1996:p%40ssw0rd%279%27%21@cluster0.eqd9ks9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-//     const client = new MongoClient(uri);
-
-//   try {
-//     await client.connect();
-//     console.log("Connected successfully to server");
-//     //const database = client.db("your_database_name"); // Replace with your database name
-//     // Perform operations here
-//   } catch (e) {
-//     console.error(e);
-//   } finally {
-//     await client.close();
-//   }
-// }
-
-// main().catch(console.error);
-
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
@@ -28,10 +10,8 @@ var connection = mysql.createConnection({
   database : 'node_project'
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+
+
 
 
 
@@ -40,26 +20,27 @@ const port = process.env.PORT || 4000;
 
 const users = [];
 
+
 app.use(express.json())
 
 app.post("/register", (req , res) => {
     try{
         let { email , password } = req.body
         //find user
-        let sql1 = `SELECT * From users where email = '${email}'`;
-        let findUser =  connection.query(sql1);
-        if(findUser){
-            res.status(400).send("wrong email or password !");
-            return;
-        }
-                // insert statment
-        let sql2 = `INSERT INTO users(email,password)
-        VALUES('${email}','${password}')`;
-
-        // execute the insert statment
-        connection.query(sql2);
-        users.push({email,password});
-        res.send(201).send("registered successfully !");
+            connection.query("SELECT * FROM users WHERE email = ?", [email], function (err, result) {
+              if(result.length > 0){
+                res.status(400).send("wrong email or password !");
+            }else{
+            // insert statment
+            let sql2 = `INSERT INTO users(email,password)
+            VALUES('${email}','${password}')`;
+            // execute the insert statment
+            connection.query(sql2);
+            connection.query("SELECT * FROM users WHERE email = ?", [email], function (err, result) {
+                res.send({"message":"registered successfully !","user":result});              
+            });
+            }
+            });
     } catch(err){
         res.status(500).send({message: err.message })
     }
@@ -69,25 +50,118 @@ app.post("/login" , (req , res)=>{
     try{
         let { email , password } = req.body
         //find user
-        let sql1 = `SELECT * From users where email = '${email}'`;
-        let findUser =  connection.query(sql1);
-        // if(!findUser){
-        if (findUser){
-            res.status(400).send("wrong email or password !")
-            return
-        }
+        connection.query("SELECT * FROM users WHERE email = ? and password = ?", [email,password], function (err, result) {
+            if(result.length > 0){
+                res.send({"message":"login successfully !","user":result});              
+          }else{
+            res.status(400).send("wrong email or password !");
 
-        if(password  == findUser.password){
-            res.send(201).send(findUser.password);
-        }else{
-            res.status(400).send("wrong email or password !")
-        }
+          }
+          });
+
+
+
+    }catch(err){
+        res.status(500).send({message: err.message })
+    }
+}) 
+
+app.post("/add_restaurant" , (req , res)=>{
+
+    try{
+        let { name , location } = req.body
+        //find user
+            // insert statment
+            let sql2 = `INSERT INTO restaurants(name,location)
+            VALUES('${name}','${location}')`;
+            // execute the insert statment
+            connection.query(sql2);
+            connection.query("SELECT * FROM restaurants WHERE name = ?", [name], function (err, result) {
+                res.send({"message":"restaurant added successfully !","restaurant":result});              
+            });
+    } catch(err){
+        res.status(500).send({message: err.message })
+    }
+})
+
+app.get("/show_restaurant" , (req , res)=>{
+    try{
+        //find user
+        connection.query("SELECT * FROM restaurants", function (err, result) {
+            res.send({"message":"all restaurants","restaurant":result});              
+          });
     }catch(err){
         res.status(500).send({message: err.message })
     }
 })
 
+app.post("/add_meal" , (req , res)=>{
+
+    try{
+        let { name , restaurant_id , price } = req.body
+        //find user
+            // insert statment
+            let sql2 = `INSERT INTO meals(name,restaurant_id,price)
+            VALUES('${name}','${restaurant_id}','${price}')`;
+            // execute the insert statment
+            connection.query(sql2);
+            connection.query("SELECT * FROM meals WHERE name = ? and restaurant_id", [name,restaurant_id], function (err, result) {
+                res.send({"message":"meal added successfully !","meal":result});              
+            });
+    } catch(err){
+        res.status(500).send({message: err.message })
+    }
+})
+
+app.get("/show_all_meals" , (req , res)=>{
+    try{
+        //find user
+        connection.query("SELECT * FROM meals", function (err, result) {
+            res.send({"message":"all meals","meal":result});              
+          });
+    }catch(err){
+        res.status(500).send({message: err.message })
+    }
+})
+
+app.get("/show_restaurant_meals" , (req , res)=>{
+    try{
+        let { restaurant_id } = req.body
+        //find user
+        connection.query("SELECT * FROM meals where restaurant_id = ?" , [restaurant_id], function (err, result) {
+            res.send({"message":"restaurant meals","meal":result});              
+          });
+    }catch(err){
+        res.status(500).send({message: err.message })
+    }
+})
+
+app.post("/order" , (req , res)=>{
+    try{
+        let sections = req.body.order;
+        connection.query("SELECT MAX(orderID) FROM orders" , function (err, result) {
+            if(!result[0]['MAX(orderID)']){
+                sections.forEach(element => {
+                    connection.query("INSERT INTO orders(users_id,restaurant_id,meal_id,num,orderID) VALUES(?,?,?,?,?)"
+                     , [element.user_id,element.restaurant_id,element.meal_id,element.num,1], function (err, result) {
+                    });            
+                });
+            }else{
+                let orderID = result[0]['MAX(orderID)']+1;
+                sections.forEach(element => {
+                    connection.query("INSERT INTO orders(users_id,restaurant_id,meal_id,num,orderID) VALUES(?,?,?,?,?)"
+                     , [element.user_id,element.restaurant_id,element.meal_id,element.num,orderID], function (err, result) {
+                    });            
+                });
+            }
+            res.send({"message":"order add successfully"});              
+        });
+        //find user
+    } catch(err){
+        res.status(500).send({message: err.message })
+    }
+})
 
 app.listen(port, () => {
-    console.log("server is started on port 3000")
+    console.log("server is started on port 4000")
 })
