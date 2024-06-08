460964d0996e262298d41c3a936210cd13003a94
slimanasaad <106686625+slimanasaad@users.noreply.github.com>
2024-06-08 12:02:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,9 +8,6 @@ const connection = mysql.createConnection({
   user     : process.env.DB_USERNAME,
   password : process.env.DB_PASSWORD,
   database : process.env.DB_DBNAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
 });
 
 
